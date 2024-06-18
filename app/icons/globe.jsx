4f31bb80d62a4cd6102114d0f49cef3b8c967a94
heronhoga <nozzeravixer@gmail.com>
2024-06-18 23:47:33 +0700

    init

---
+++ app/icons/globe.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function FiGlobe(props) {
+  return <svg stroke="currentColor" fill="none" strokeWidth={2} viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" {...props}><circle cx={12} cy={12} r={10} /><line x1={2} y1={12} x2={22} y2={12} /><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" /></svg>;
+}
+
+export default FiGlobe;
