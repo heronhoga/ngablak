ca7a19051fb683d4e0af5d243b321fa1ee363be5
heronhoga <nozzeravixer@gmail.com>
2024-06-19 21:41:10 +0700

    fix:fix navbar layout

--- app/created-components/Navbar.tsx
+++ app/created-components/Navbar.tsx
@@ -13,64 +13,64 @@ function Navbar() {
   };
 
   return (
-    <div className="py-2 after:font-semibold flex items-center justify-between bg-blue-950 font-semibold rounded-b-[2.5rem] shadow-sm md:shadow-lg text-sm md:text-lg lg:text-2xl text-white">
+    <div className="py-2 flex items-center justify-between bg-blue-950 font-semibold rounded-b-[2.5rem] shadow-sm md:shadow-lg text-sm md:text-lg lg:text-xl text-white">
       <div className="flex items-center">
         <Image
           src="/images/logo-kabupaten.png"
           alt="Logo"
-          width={961}
+          width={448}
           height={682}
           className="h-12 w-20 md:h-24 md:w-40"
-        ></Image>
-        <div className="hidden md:flex">
-          <h1>DESA NGABLAK</h1>
-          <Link href="/" className="mx-1 md:mx-3">
-            Beranda
-          </Link>
-          <Link href="/profil" className="mx-1 md:mx-3">
-            Profil Desa
-          </Link>
-          <Link href="/berita" className="mx-1 md:mx-3">
-            Berita
-          </Link>
-          <Link href="/kelembagaan" className="mx-1 md:mx-3">
-            Kelembagaan
-          </Link>
-          <Link href="/pemberdayaan" className="mx-1 md:mx-3">
-            Pemberdayaan
-          </Link>
-          <Link href="/layanan" className="mx-1 md:mx-3">
-            Layanan
-          </Link>
-          <Link href="/kontak" className="mx-1 md:mx-3">
-            Kontak Kami
-          </Link>
-        </div>
+        />
+        <h1 className="ml-2 md:ml-4">DESA NGABLAK</h1>
       </div>
-      <div className="md:hidden" onClick={toggleMenu}>
+      <div className="hidden xl:flex">
+        <Link href="/" className="mx-1 md:mx-3">
+          Beranda
+        </Link>
+        <Link href="/profil" className="mx-1 md:mx-3">
+          Profil Desa
+        </Link>
+        <Link href="/berita" className="mx-1 md:mx-3">
+          Berita
+        </Link>
+        <Link href="/kelembagaan" className="mx-1 md:mx-3">
+          Kelembagaan
+        </Link>
+        <Link href="/pemberdayaan" className="mx-1 md:mx-3">
+          Pemberdayaan
+        </Link>
+        <Link href="/layanan" className="mx-1 md:mx-3">
+          Layanan
+        </Link>
+        <Link href="/kontak" className="ml-1 mr-1 md:ml-3 md:mr-10">
+          Kontak Kami
+        </Link>
+      </div>
+      <div className="xl:hidden mr-8 z-10" onClick={toggleMenu}>
         {isOpen ? <Close size={24} /> : <GiHamburgerMenu size={24} />}
       </div>
       {isOpen && (
-        <div className="flex flex-col md:hidden">
-          <Link href="/" className="my-2" onClick={toggleMenu}>
+        <div className="flex flex-col xl:hidden absolute top-16 md:top-10 lg:top-11 left-0 w-full bg-blue-950 z-0">
+          <Link href="/" className="m-3" onClick={toggleMenu}>
             Beranda
           </Link>
-          <Link href="/profil" className="my-2" onClick={toggleMenu}>
+          <Link href="/profil" className="m-3" onClick={toggleMenu}>
             Profil Desa
           </Link>
-          <Link href="/berita" className="my-2" onClick={toggleMenu}>
+          <Link href="/berita" className="m-3" onClick={toggleMenu}>
             Berita
           </Link>
-          <Link href="/kelembagaan" className="my-2" onClick={toggleMenu}>
+          <Link href="/kelembagaan" className="m-3" onClick={toggleMenu}>
             Kelembagaan
           </Link>
-          <Link href="/pemberdayaan" className="my-2" onClick={toggleMenu}>
+          <Link href="/pemberdayaan" className="m-3" onClick={toggleMenu}>
             Pemberdayaan
           </Link>
-          <Link href="/layanan" className="my-2" onClick={toggleMenu}>
+          <Link href="/layanan" className="m-3" onClick={toggleMenu}>
             Layanan
           </Link>
-          <Link href="/kontak" className="my-2" onClick={toggleMenu}>
+          <Link href="/kontak" className="m-3" onClick={toggleMenu}>
             Kontak Kami
           </Link>
         </div>
