9f97ca7bb47afaefbf8bfc10b187fd5a0af8b1a8
heronhoga <nozzeravixer@gmail.com>
2024-08-06 00:12:55 +0700

    feat:add layanan publik content

--- app/data/layanan-publik.ts
+++ app/data/layanan-publik.ts
@@ -6,6 +6,7 @@ interface Requirement {
 interface Section {
   name: string;
   requirements: Requirement[];
+  catatan?: string[];
 }
 
 interface PublicService {
@@ -22,11 +23,7 @@ const publicServices: PublicService[] = [
         requirements: [
           { id: 1, description: "Formulir F-1.15 atau F-1.16" },
           { id: 2, description: "Surat Keterangan Pindah" },
-          {
-            id: 3,
-            description:
-              "Fotokopi Surat Nikah/Kutipan Akta Perkawinan/Perceraian",
-          },
+          { id: 3, description: "Fotokopi Surat Nikah/Kutipan Akta Perkawinan/Perceraian" },
         ],
       },
       {
@@ -51,11 +48,7 @@ const publicServices: PublicService[] = [
         requirements: [
           { id: 1, description: "Formulir F-1.15 atau F-1.16" },
           { id: 2, description: "Kartu Keluarga Lama Asli" },
-          {
-            id: 3,
-            description:
-              "Fotokopi Kutipan Kematian/Surat Pindah/Data Dukung Lainnya",
-          },
+          { id: 3, description: "Fotokopi Kutipan Kematian/Surat Pindah/Data Dukung Lainnya" },
         ],
       },
       {
@@ -66,10 +59,7 @@ const publicServices: PublicService[] = [
           { id: 3, description: "Surat Kehilangan dari Polsek" },
           { id: 4, description: "Dokumen Pendukung" },
           { id: 5, description: "Fotokopi Akta Kelahiran" },
-          {
-            id: 6,
-            description: "Fotokopi Akta Perkawinan/Perceraian/Buku Nikah",
-          },
+          { id: 6, description: "Fotokopi Akta Perkawinan/Perceraian/Buku Nikah" },
         ],
       },
     ],
@@ -80,11 +70,7 @@ const publicServices: PublicService[] = [
       {
         name: "KTP Baru",
         requirements: [
-          {
-            id: 1,
-            description:
-              "Berusia 17 tahun keatas, Sudah Kawin, atau Pernah Kawin",
-          },
+          { id: 1, description: "Berusia 17 tahun keatas, Sudah Kawin, atau Pernah Kawin" },
           { id: 2, description: "Fotokopi Kartu Keluarga" },
           { id: 3, description: "SKDLN dari Instansi Pelaksana" },
         ],
@@ -102,27 +88,15 @@ const publicServices: PublicService[] = [
         requirements: [
           { id: 1, description: "Fotokopi Kartu Keluarga" },
           { id: 2, description: "KTP-el Lama" },
-          {
-            id: 3,
-            description:
-              "Surat Keterangan Perubahan Kependudukan dan Peristiwa Penting",
-          },
+          { id: 3, description: "Surat Keterangan Perubahan Kependudukan dan Peristiwa Penting" },
         ],
       },
       {
         name: "Orang Asing yang Berijin",
         requirements: [
-          {
-            id: 1,
-            description:
-              "Berusia 17 (dewasa) keatas, Sudah Kawin, atau Pernah Kawin",
-          },
+          { id: 1, description: "Berusia 17 (dewasa) keatas, Sudah Kawin, atau Pernah Kawin" },
           { id: 2, description: "Fotokopi Kartu Keluarga" },
-          {
-            id: 3,
-            description:
-              "Belum Menikah Melampirkan Kutipan Akta Kelahiran, Paspor, dan Kartu Izin Tetap",
-          },
+          { id: 3, description: "Belum Menikah Melampirkan Kutipan Akta Kelahiran, Paspor, dan Kartu Izin Tetap" },
           { id: 4, description: "Surat Catatan Kepolisian" },
         ],
       },
@@ -162,12 +136,9 @@ const publicServices: PublicService[] = [
         name: "Antar Kabupaten/Kota",
         requirements: [
           { id: 1, description: "KTP-el" },
-          {
-            id: 1,
-            description: "Surat Keterangan Kehilangan apabila KTP-el Hilang",
-          },
-          { id: 1, description: "Surat Keterangan Pindah" },
-          { id: 1, description: "Pengantar RT/RW Setempat" },
+          { id: 2, description: "Surat Keterangan Kehilangan apabila KTP-el Hilang" },
+          { id: 3, description: "Surat Keterangan Pindah" },
+          { id: 4, description: "Pengantar RT/RW Setempat" },
         ],
       },
     ],
@@ -178,35 +149,13 @@ const publicServices: PublicService[] = [
       {
         name: "Antar Desa/Kecamatan/Kabupaten/Provinsi",
         requirements: [
-          {
-            id: 1,
-            description: "Form Permohonan Pindah (Antar Kabupaten/Provinsi)",
-          },
-          {
-            id: 2,
-            description: "Mengisi Formulir Pindah (Antar Desa/Kecamatan)",
-          },
-          {
-            id: 3,
-            description:
-              "Fotokopi Kartu Keluarga (KK Asli apabila pengajuan satu keluarga) (Antar Desa)",
-          },
-          {
-            id: 4,
-            description: "KTP-el Asli dan Fotokopi",
-          },
-          {
-            id: 5,
-            description: "Fotokopi Buku Nikah",
-          },
-          {
-            id: 6,
-            description: "Fotokopi Surat Cerai",
-          },
-          {
-            id: 7,
-            description: "Akta Kelahiran atau SPTJM",
-          },
+          { id: 1, description: "Form Permohonan Pindah (Antar Kabupaten/Provinsi)" },
+          { id: 2, description: "Mengisi Formulir Pindah (Antar Desa/Kecamatan)" },
+          { id: 3, description: "Fotokopi Kartu Keluarga (KK Asli apabila pengajuan satu keluarga) (Antar Desa)" },
+          { id: 4, description: "KTP-el Asli dan Fotokopi" },
+          { id: 5, description: "Fotokopi Buku Nikah" },
+          { id: 6, description: "Fotokopi Surat Cerai" },
+          { id: 7, description: "Akta Kelahiran atau SPTJM" },
         ],
       },
       {
@@ -223,10 +172,7 @@ const publicServices: PublicService[] = [
       {
         name: "Pindah Datang Orang Asing Sementara/Tetap",
         requirements: [
-          {
-            id: 1,
-            description: "Surat Keterangan Pindah yang Diketahui Camat",
-          },
+          { id: 1, description: "Surat Keterangan Pindah yang Diketahui Camat" },
           { id: 2, description: "Fotokopi Paspor" },
           { id: 3, description: "Fotokopi Izin Tinggal Tetap" },
           { id: 4, description: "SKCK" },
@@ -235,22 +181,52 @@ const publicServices: PublicService[] = [
       {
         name: "Syarat Pindah Datang",
         requirements: [
-          {
-            id: 1,
-            description: "Surat Keterangan Pindah Datang dari Desa",
-          },
-          {
-            id: 2,
-            description: "Surat Keterangan Pindah WNI dari Daerah Asal",
-          },
-          {
-            id: 3,
-            description: "KTP-el",
-          },
+          { id: 1, description: "Surat Keterangan Pindah Datang dari Desa" },
+          { id: 2, description: "Surat Keterangan Pindah WNI dari Daerah Asal" },
+          { id: 3, description: "KTP-el" },
         ],
       },
-      //KARTU IDENTITAS ANAK
-      //KARTU/KETERANGAN TINGGAL SEMENTARA
+    ],
+  },
+  {
+    name: "Kartu Identitas Anak",
+    sections: [
+      {
+        name: "Penerbitan KIA WNI/Luar Negeri",
+        requirements: [
+          { id: 1, description: "Mengisi Formulir Penerbitan KIA WNI/Luar Negeri" },
+          { id: 2, description: "Fotokopi Kutipan Akta Kelahiran" },
+          { id: 3, description: "Fotokopi KK Orang Tua/Wali" },
+          { id: 4, description: "Fotokopi KTP-el" },
+          { id: 5, description: "Foto Berwarna Ukuran 4x6 (2 Lembar) untuk Anak Usia > 5 Tahun s/d 17 Tahun" },
+        ],
+        catatan: [
+          "Apabila Kasus KIA Hilang Membawa Surat Kehilangan dari Kepolisian",
+          "Apabila Kasus KIA Rusak Membawa KIA yang Rusak",
+          "Apabila Kasus Pindah Datang Membawa KIA Daerah Asal",
+          "Apabila Kasus KIA Orang Asing Membawa Fotokopi Paspos dan Ijin Tetap",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Kartu/Keterangan Tinggal Sementara",
+    sections: [
+      {
+        name: "",
+        requirements: [
+          { id: 1, description: "Formulir Sesuai Peraturan Menteri Dalam Negeri No. 14 Tahun 2015" },
+          { id: 2, description: "KTP-el" },
+          { id: 3, description: "Kartu Keluarga" },
+          { id: 4, description: "Dokumen Pendukung" },
+        ],
+        catatan: [
+          "Surat Tugas",
+          "Surat Keterangan dari Instansi Pendidikan dan Perusahaan",
+          "Surat Keterangan Berobat",
+          "Surat Pengantar dari RT/RW",
+        ],
+      },
     ],
   },
 ];
--- app/layanan/publik/page.tsx
+++ app/layanan/publik/page.tsx
@@ -1,11 +1,60 @@
 import React from "react";
 import Navbar from "@/app/created-components/Navbar";
+import Footer from "@/app/created-components/Footer";
+import publicServices from "../../data/layanan-publik";
 
 function Publik() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* NAVBAR */}
       <Navbar />
+      <div className="flex-grow p-4">
+        {/* TITLE */}
+        <div id="title" className="mt-10 text-center">
+          <h1 className="font-bold italic text-2xl md:text-3xl lg:text-4xl">
+            LAYANAN MASYARAKAT - DESA NGABLAK
+          </h1>
+          <hr className="mx-auto mt-3 w-3/4 md:w-1/2 lg:w-1/3" />
+          <h2 className="mt-3 text-sm md:text-lg lg:text-xl">
+            Kecamatan Wonosamodro, Kabupaten Boyolali
+          </h2>
+          <hr className="mx-auto mt-3 w-3/4 md:w-1/2 lg:w-1/3" />
+        </div>
+        
+        {publicServices.map((service, serviceIndex) => (
+          <div key={serviceIndex} className="mb-6">
+            
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-center mt-3">{service.name}</h2>
+            <hr className="my-3" />
+            {service.sections.map((section, sectionIndex) => (
+              <div
+                key={sectionIndex}
+                className="mb-4 p-4 border border-black rounded-lg shadow-sm mx-auto max-w-full md:max-w-2xl lg:max-w-3xl"
+              >
+                <h3 className="text-lg md:text-xl lg:text-2xl font-semibold mb-2">{section.name}</h3>
+                <h4 className="font-semibold">Persyaratan:</h4>
+                <ul className="list-disc list-inside">
+                  {section.requirements.map((requirement) => (
+                    <li key={requirement.id}>{requirement.description}</li>
+                  ))}
+                </ul>
+                {section.catatan && (
+                  <div className="mt-2">
+                    <h4 className="font-semibold">Catatan:</h4>
+                    <ul className="list-disc list-inside">
+                      {section.catatan.map((note, noteIndex) => (
+                        <li key={noteIndex}>{note}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {/* FOOTER */}
+      <Footer />
     </div>
   );
 }
